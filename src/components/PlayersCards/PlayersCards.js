319e1fca30a51bbe1cdeef47ca7af9048a103619
Naimul Alam <naimul2707@gmail.com>
2021-03-01 18:40:59 +0100

    finalize

--- src/components/PlayersCards/PlayersCards.js
+++ src/components/PlayersCards/PlayersCards.js
@@ -1,6 +1,7 @@
 import Player from '../Player/Player';
 import fakeData from '../../fakeData/fakeData.json';
 import { useEffect, useState } from 'react';
+import './PlayersCards.css'
 
 const PlayersCards = (props) => {
 
@@ -14,6 +15,9 @@ const PlayersCards = (props) => {
 
     return (
         <section className="players col-md-8 border-end">
+            <div className="player-main">
+                <h1>Build Your Dream Team </h1>
+            </div>
             <div className="row row-cols-3 gx-5 gy-5">
                 {
                     players.map(player => <Player addToCartHandler={props.addToCartHandler} key={player.id} player={player} />)
